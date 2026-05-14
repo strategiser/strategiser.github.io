9fd908815318540e4c7c5ac7a60da75eedfa59fa
strategiser <198572396+strategiser@users.noreply.github.com>
2026-05-14 11:29:19 +1000

    Add event listener for log workout button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const logWorkoutBtn = document.getElementById("logWorkoutBtn");
+
+  logWorkoutBtn.addEventListener("click", () => {
+    alert("Log workout button clicked!");
+  });
+});
+
 
